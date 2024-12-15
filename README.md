debb3cb17d3bef31dbae8318422567000b354af2
Som Sothea <somsothea@gmail.com>
2024-12-16 00:04:55 +0700

    Upload Backend API
    Express JS

---
+++ README.md
@@ -0,0 +1,19 @@
+# Express Server with Docker Compose
+
+## Setup ENV
+
+```sh
+cp .env.example .env
+```
+
+## Build Project
+
+```sh
+docker compose build
+```
+
+## Run Project
+
+```sh
+docker compose up -d
+```
---
+++ fixture.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const { faker } = require('@faker-js/faker');
+const dbConnect = require('./src/db/db.js');
+const StoreModel = require('./src/models/Store.js');
+const UserModel = require('./src/models/user.js');
+const ItemModel = require('./src/models/Item.js');
+
+dbConnect().catch((err) => {
+  console.log(err);
+});
+
+const numberOfStore = 1000;
+const numberOfUsers = 100;
+const numberOfItems = 500;
+
+console.log(Date.now());
+
+async function generate() {
+  for (let i = 0; i < numberOfStore; i++) {
+    const newStore = new StoreModel({
+      price: faker.commerce.price(),
+      title: faker.lorem.sentence(5),
+      category: faker.music.genre(),
+      author: faker.person.fullName(),
+    });
+    const result = await newStore.save();
+    console.log(`${i} - Store with id: ${result._id} generated`);
+  }
+
+  const usersList = [];
+
+  for (let i = 0; i < numberOfUsers; i++) {
+    const newUser = new UserModel({
+      username: faker.internet.userName(),
+      firstname: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      dateOfBirth: faker.date.birthdate(),
+      email: faker.internet.email(),
+    });
+    const result = await newUser.save();
+    usersList.push(result._id);
+    console.log(`${i} - User with id: ${result._id} generated`);
+  }
+
+  for (let i = 0; i < numberOfItems; i++) {
+    const randomId = usersList[Math.floor(Math.random() * usersList.length)];
+    const newItem = new ItemModel({
+      author: randomId,
+      page: faker.number.int({ max: 500 }),
+      description: faker.lorem.paragraph(),
+      genre: faker.lorem.word(),
+      title: faker.lorem.sentence({ min: 3, max: 5 }),
+    });
+    const result = await newItem.save();
+    console.log(`${i} - Item with id: ${result._id} generated`);
+  }
+}
+generate();
